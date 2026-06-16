2102a0e3131ed711ab764049554f7cb333cc3e09
Xylia <xy231153nr@gmail.com>
2026-06-16 23:17:50 +0100

    Update GUI.c

--- src/GUI.c
+++ src/GUI.c
@@ -2,5 +2,10 @@
 #include"setup.h"
 int main(int argc,char**argv)
 {
+  Gtk_application_new*app;
+  app=gtk_aplication_new("org.gtk.LST",G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app,"activate",G_CALLBACK(activate),NULL);
+  status=g_application_run(G_APPLICATION(app),argc,argv);
+  g_object_unref(app);
   return status;
 }
