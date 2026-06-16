67fc4314393e1743a27548ecf55e250fe8c81422
Xylia <xy231153nr@gmail.com>
2026-06-16 21:35:17 +0100

    Update AURVirusRemover.sh

--- AURVirusRemover.sh
+++ AURVirusRemover.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-curl -o BadAURPackages https://md.archlinux.org/s/SxbqukK6IA
-PackageString()={};
-while IFS=read -r PackageString; do
-  if[[ -n "$PackageString" ]]; then
-    echo "Removing $PackageString..."
-    sudo pacman -Rsn "$PackageString"
-  fi
+cd /tmp
+wget -q https://github.com/EricChiang/pup/releases/download/v0.4.0/pup_linux_amd64.zip
+unzip -q pup_linux_amd64.zip
+sudo mv pup /usr/local/bin/
+pup --version
+curl -s https://md.archlinux.org/s/SxbqukK6IA | \
+    pup 'div.markdown-body text{}' | \
+    sed '1d;$d' > BadAURPackages
+while IFS= read -r PackageString; do
+    if [[ -n "$PackageString" ]]; then
+        echo "Removing $PackageString..."
+        sudo pacman -Rsn "$PackageString"
+    fi
 done < BadAURPackages
