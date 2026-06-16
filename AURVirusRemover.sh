2fcead06d553332f2125d5ea30b186fb84f2fcce
Xylia <xy231153nr@gmail.com>
2026-06-16 21:10:22 +0100

    Update AURVirusRemover.sh

--- AURVirusRemover.sh
+++ AURVirusRemover.sh
@@ -1,7 +1,9 @@
-#!bin/bash
+#!/bin/bash
 curl -o BadAURPackages https://md.archlinux.org/s/SxbqukK6IA
 PackageString()={};
-while(line !=0;)
-{
-  sudo pacman -Rsn $PackageString;
-}
+while IFS=read -r PackageString; do
+  if[[ -n "$PackageString" ]]; then
+    echo "Removing $PackageString..."
+    sudo pacman -Rsn "$PackageString"
+  fi
+done < BadAURPackages
